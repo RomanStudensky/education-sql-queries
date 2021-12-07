4567260c18cd2747e49a09658c6abacefcd1aa66
Roman.Studensky <roman.studensky@waveaccess.ru>
2021-12-07 15:25:32 +0300

    Init commit

---
+++ src/main/java/com/romanstudensky/education/sqlqueries/SqlQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.romanstudensky.education.sqlqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlQueriesApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/sql-queries/queries.sql
@@ -0,0 +1,3 @@
+select id_akt
+from akt_spis
+where id_akt = '1'
